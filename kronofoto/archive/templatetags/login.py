fe885fd7b0065abffab33620c822f7fbb8466326
jonathan <jonathan.voss@uni.edu>
2020-04-03 18:06:53 -0500

    Fix login
    Hijacking post in a templatetag doesn't actually work.

--- kronofoto/archive/templatetags/login.py
+++ kronofoto/archive/templatetags/login.py
@@ -4,14 +4,8 @@ from django.contrib.auth import login
 
 register = template.Library()
 
-@register.inclusion_tag('registration/login.html', takes_context=True)
+@register.inclusion_tag('archive/login.html', takes_context=True)
 def make_login(context):
     request = context['request']
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, form.get_user())
-    else:
-        form = AuthenticationForm(request)
+    form = AuthenticationForm(request)
     return { 'form': form, 'user': request.user, 'request': request}
