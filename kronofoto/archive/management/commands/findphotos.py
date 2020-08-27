eabb0b57e4483d881a861117166e90744531e6d2
Jonathan Voss <jonathan.voss@uni.edu>
2020-08-27 16:00:02 -0500

    Add each photo within a transaction

--- kronofoto/archive/management/commands/findphotos.py
+++ kronofoto/archive/management/commands/findphotos.py
@@ -4,6 +4,7 @@ from archive.models import CSVRecord, Photo, Collection, Donor
 import sys
 import os
 import shutil
+from django.db import transaction
 
 
 def splitname(name):
@@ -26,29 +27,33 @@ class Command(BaseCommand):
         files = {os.path.splitext(f)[0]: os.path.join(d, f) for d, _, fs in os.walk(options['photo_archive'][0]) for f in fs if not f.startswith('.')}
         for record in CSVRecord.objects.filter(photo__isnull=True):
             if record.filename in files:
-                fname = ''
-                filename = files[record.filename]
-                photographer = record.photographer.strip()
-                scanner = splitname(record.scanner)
-                scanner, _ = Donor.objects.get_or_create(first_name=scanner[1], last_name=scanner[0])
-                donor, _ = Donor.objects.get_or_create(first_name=record.donorFirstName, last_name=record.donorLastName)
-                photo = Photo(
-                    donor=donor,
-                    city=record.city,
-                    county=record.county,
-                    state=record.state,
-                    country=record.country,
-                    year=record.year,
-                    caption=record.comments,
-                    is_published=True,
-                    photographer=photographer,
-                    scanner=scanner,
-                )
-                fname = 'original/{}.jpg'.format(photo.uuid)
-                shutil.copyfile(filename, os.path.join(settings.MEDIA_ROOT, fname))
-                photo.original.name = fname
-                photo.save()
-                photo.created = record.added_to_archive
-                photo.save()
-                record.photo = photo
-                record.save()
+                with transaction.atomic():
+                    try:
+                        fname = ''
+                        filename = files[record.filename]
+                        photographer = record.photographer.strip()
+                        scanner = splitname(record.scanner)
+                        scanner, _ = Donor.objects.get_or_create(first_name=scanner[1], last_name=scanner[0])
+                        donor, _ = Donor.objects.get_or_create(first_name=record.donorFirstName, last_name=record.donorLastName)
+                        photo = Photo(
+                            donor=donor,
+                            city=record.city,
+                            county=record.county,
+                            state=record.state,
+                            country=record.country,
+                            year=record.year,
+                            caption=record.comments,
+                            is_published=True,
+                            photographer=photographer,
+                            scanner=scanner,
+                        )
+                        fname = 'original/{}.jpg'.format(photo.uuid)
+                        shutil.copyfile(filename, os.path.join(settings.MEDIA_ROOT, fname))
+                        photo.original.name = fname
+                        photo.save()
+                        photo.created = record.added_to_archive
+                        photo.save()
+                        record.photo = photo
+                        record.save()
+                    except:
+                        print(record.filename, 'failure')
