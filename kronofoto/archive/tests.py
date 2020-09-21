252d84c36e075c85b2d577028f66d0e80e542b3a
jonathan <jonathan.voss@uni.edu>
2020-09-21 15:55:41 -0500

    Rewrite the timeline test
    It was helpful when writing the function, but it was extremely brittle.
    Now it only tests the structure of the timeline a bit.

--- kronofoto/archive/tests.py
+++ kronofoto/archive/tests.py
@@ -571,116 +571,28 @@ class ParserTest(SimpleTestCase):
 
 from archive.templatetags import timeline
 class TimelineDisplay(SimpleTestCase):
+    def assertIsPosition(self, obj):
+        for key in ('x', 'y', 'width', 'height'):
+            self.assertIn(key, obj)
+            self.assertTrue(isinstance(obj[key], int) or isinstance(obj[key], float))
     def testShouldDefineMinorMarkerPositions(self):
         years = [(year, '/{}'.format(year), '/{}.json'.format(year)) for year in [1900, 1901, 1902, 1903, 1904, 1905]]
         result = timeline.make_timeline(years, width=60)
-        self.assertEqual(result['majornotches'], [{
-            'target': '/1900',
-            'json_target': '/1900.json',
-            'box': {
-                'x': 0,
-                'y': 5,
-                'width': 10,
-                'height': 5,
-            },
-            'notch': {
-                'x': 0,
-                'y': 5,
-                'width': 2,
-                'height': 5,
-            },
-            'label': {
-                'text': '1900',
-                'x': 5,
-                'y': 3
-            }
-        },
-        {
-            'target': '/1905',
-            'json_target': '/1905.json',
-            'box': {
-                'x': 50,
-                'y': 5,
-                'width': 10,
-                'height': 5,
-            },
-            'notch': {
-                'x': 50,
-                'y': 5,
-                'width': 2,
-                'height': 5,
-            },
-            'label': {
-                'text': '1905',
-                'x': 55,
-                'y': 3
-            }
-        },
-        ])
-        self.assertEqual(result['minornotches'], [{
-            'target': '/1901',
-            'json_target': '/1901.json',
-            'box': {
-                'x': 10,
-                'y': 5,
-                'width': 10,
-                'height': 5,
-            },
-            'notch': {
-                'x': 10,
-                'y': 7,
-                'width': 2,
-                'height': 3,
-            }
-        },
-        {
-            'target': '/1902',
-            'json_target': '/1902.json',
-            'box': {
-                'x': 20,
-                'y': 5,
-                'width': 10,
-                'height': 5,
-            },
-            'notch': {
-                'x': 20,
-                'y': 7,
-                'width': 2,
-                'height': 3,
-            }
-        },
-        {
-            'target': '/1903',
-            'json_target': '/1903.json',
-            'box': {
-                'x': 30,
-                'y': 5,
-                'width': 10,
-                'height': 5,
-            },
-            'notch': {
-                'x': 30,
-                'y': 7,
-                'width': 2,
-                'height': 3,
-            }
-        },
-        {
-            'target': '/1904',
-            'json_target': '/1904.json',
-            'box': {
-                'x': 40,
-                'y': 5,
-                'width': 10,
-                'height': 5,
-            },
-            'notch': {
-                'x': 40,
-                'y': 7,
-                'width': 2,
-                'height': 3,
-            }
-        },
-        ])
+        self.assertIn('majornotches', result)
+        self.assertEqual(len(result['majornotches']), 1)
+        for notch in result['majornotches']:
+            for key in ('target', 'json_target', 'box', 'notch', 'label'):
+                self.assertIn(key, notch)
+            for key in ('box', 'notch'):
+                self.assertIsPosition(notch[key])
+            for key in ('text', 'x', 'y'):
+                self.assertIn(key, notch['label'])
+        self.assertIn('minornotches', result)
+        self.assertEqual(len(result['minornotches']), 5)
+        for notch in result['minornotches']:
+            for key in ('target', 'json_target', 'box', 'notch'):
+                self.assertIn(key, notch)
+            for key in ('box', 'notch'):
+                self.assertIsPosition(notch[key])
 
 
