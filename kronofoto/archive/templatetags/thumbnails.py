346e5f62a2240da8b58a844fe9a55c473a768f31
jonathan <jonathan.voss@uni.edu>
2020-10-15 14:37:43 -0500

    Fix missing tags when changing photos in timeline

--- kronofoto/archive/templatetags/thumbnails.py
+++ kronofoto/archive/templatetags/thumbnails.py
@@ -2,12 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('archive/thumbnails.html')
-def make_thumbnails(this_page, prev_page, next_page, getparams):
-    context = {
-        'page': this_page,
-        'prev_page': prev_page,
-        'next_page': next_page,
-        'getparams': getparams,
-    }
+@register.inclusion_tag('archive/thumbnails.html', takes_context=True)
+def make_thumbnails(context):
     return context
