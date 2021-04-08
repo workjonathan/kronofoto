bf09d1b277208064c2dca2eeefdbe037d3a402a7
jonathan <jonathan.voss@uni.edu>
2021-04-08 14:56:22 -0500

    Recover lost example settings file

---
+++ kronofoto/kronofoto/examplesettings.py
@@ -0,0 +1,12 @@
+from .defaults import *
+# Please copy this file, do not rename it.
+
+# you can override any configuration here
+
+DEBUG = True
+
+# the SECRET_KEY is used for cryptographic signing. It's 50 random characters.
+SECRET_KEY =
+
+GOOGLE_RECAPTCHA_SECRET_KEY =
+GOOGLE_RECAPTCHA_SITE_KEY =
