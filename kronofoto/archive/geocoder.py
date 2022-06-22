9aad6d36dbb53f8f75f5aaaa5865620a6842d8db
jonathan <jonathan.voss@uni.edu>
2022-06-22 15:08:09 -0500

    Geocoding command uses cached locations

--- kronofoto/archive/geocoder.py
+++ kronofoto/archive/geocoder.py
@@ -1,10 +1,31 @@
 from django.conf import settings
-from .models import CSVRecord
+from .models import CSVRecord, Location
+from geocoding import geom
 from geocoding.google import Geocoder, GeocodeError
 import logging
 
+
+class CachingGeocoder:
+    def __init__(self, geocoder):
+        self.geocoder = geocoder
+
+    def geocode(self, description):
+        try:
+            location = Location.objects.locate(description=description)
+            return geom.Location(centroid=location.location_point, bounds=location.location_bounds)
+        except Location.DoesNotExist:
+            location = self.geocoder.geocode(description=description)
+            Location.objects.create(
+                description=description,
+                location_point=location.centroid,
+                location_bounds=location.bounds,
+            )
+            return location
+
+
+
 class CSVGeocoder:
-    def __init__(self, geocoder=Geocoder(key=settings.GOOGLE_MAPS_KEY)):
+    def __init__(self, geocoder=CachingGeocoder(geocoder=Geocoder(key=settings.GOOGLE_MAPS_KEY))):
         self.geocoder = geocoder
 
     def geocode_from_csv_data(self):
--- kronofoto/archive/models.py
+++ kronofoto/archive/models.py
@@ -629,3 +629,20 @@ class CSVRecord(models.Model):
         self.country = self.country.strip()
         self.state = self.state.strip()
         self.comments = self.comments.strip()
+
+
+class LocationQuerySet(models.QuerySet):
+    def locate(self, description):
+        return self.get(description=description)
+
+class Location(models.Model):
+    description = models.TextField(unique=True)
+    location_point = models.PointField(null=True, srid=4326, blank=True)
+    location_bounds = models.MultiPolygonField(null=True, srid=4326, blank=True)
+    objects = LocationQuerySet.as_manager()
+
+    def describe(self):
+        return self.description
+
+    def __str__(self):
+        return self.description
