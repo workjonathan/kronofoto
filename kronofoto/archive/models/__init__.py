78384106d64a8b1da50a7a7d868a66c97f10a104
jonathan <jonathan.voss@uni.edu>
2022-07-18 17:02:12 -0500

    Split photosphere models into own module

--- kronofoto/archive/models/__init__.py
+++ kronofoto/archive/models/__init__.py
@@ -21,6 +21,7 @@ import hashlib
 from django.http import QueryDict
 from os import path
 from ..storage import OverwriteStorage
+from .photosphere import PhotoSphere, PhotoSpherePair, get_photosphere_path
 
 class LowerCaseCharField(models.CharField):
     def get_prep_value(self, value):
@@ -503,39 +504,6 @@ class Photo(models.Model):
         location = {location} if location != "Location: n/a" else set()
         return terms | tags | location | { str(self.donor), "history of Iowa", "Iowa", "Iowa History" }
 
-def get_photosphere_path(instance, filename):
-    return path.join('photosphere', '{}.jpg'.format(instance.uuid))
-
-
-class PhotoSphere(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=512, blank=False)
-    image = models.ImageField(upload_to=get_photosphere_path, storage=OverwriteStorage(), null=True, editable=True)
-    heading = models.FloatField(
-        default=0,
-        validators=[MinValueValidator(limit_value=-180), MaxValueValidator(limit_value=180)],
-    )
-    photos = models.ManyToManyField(Photo, through="PhotoSpherePair")
-    location = models.PointField(null=True, srid=4326, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class PhotoSpherePair(models.Model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, help_text="Select a photo then click Save and Continue Editing to use the interactive tool")
-    photosphere = models.ForeignKey(PhotoSphere, on_delete=models.CASCADE, help_text="Select a photo and photo sphere then click Save and Continue Editing to use the interactive tool")
-    azimuth = models.FloatField(default=0, validators=[MinValueValidator(limit_value=-180), MaxValueValidator(limit_value=180)])
-    inclination = models.FloatField(default=0, validators=[MinValueValidator(limit_value=-90), MaxValueValidator(limit_value=90)])
-    distance = models.FloatField(default=500, validators=[MinValueValidator(limit_value=1), MaxValueValidator(limit_value=2000)])
-
-    class Meta:
-        verbose_name = 'Photo position'
-
-    def __str__(self):
-        return "{donor} - {fi} - {sphere}".format(donor=self.photo.donor, fi=str(self.photo), sphere=self.photosphere)
-
-
 class WordCount(models.Model):
     FIELDS = [
         ('CA', 'Caption'),
@@ -551,6 +519,7 @@ class WordCount(models.Model):
         index_together = [
             ('word', 'field')
         ]
+
 
 
 class PhotoTag(models.Model):
---
+++ kronofoto/archive/models/photosphere.py
@@ -0,0 +1,40 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.gis.db import models
+import uuid
+from os import path
+from ..storage import OverwriteStorage
+
+
+def get_photosphere_path(instance, filename):
+    return path.join('photosphere', '{}.jpg'.format(instance.uuid))
+
+
+class PhotoSphere(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=512, blank=False)
+    image = models.ImageField(upload_to=get_photosphere_path, storage=OverwriteStorage(), null=True, editable=True)
+    heading = models.FloatField(
+        default=0,
+        validators=[MinValueValidator(limit_value=-180), MaxValueValidator(limit_value=180)],
+    )
+    photos = models.ManyToManyField("Photo", through="PhotoSpherePair")
+    location = models.PointField(null=True, srid=4326, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PhotoSpherePair(models.Model):
+    photo = models.ForeignKey("Photo", on_delete=models.CASCADE, help_text="Select a photo then click Save and Continue Editing to use the interactive tool")
+    photosphere = models.ForeignKey(PhotoSphere, on_delete=models.CASCADE, help_text="Select a photo and photo sphere then click Save and Continue Editing to use the interactive tool")
+    azimuth = models.FloatField(default=0, validators=[MinValueValidator(limit_value=-180), MaxValueValidator(limit_value=180)])
+    inclination = models.FloatField(default=0, validators=[MinValueValidator(limit_value=-90), MaxValueValidator(limit_value=90)])
+    distance = models.FloatField(default=500, validators=[MinValueValidator(limit_value=1), MaxValueValidator(limit_value=2000)])
+
+    class Meta:
+        verbose_name = 'Photo position'
+
+    def __str__(self):
+        return "{donor} - {fi} - {sphere}".format(donor=self.photo.donor, fi=str(self.photo), sphere=self.photosphere)
+
+
