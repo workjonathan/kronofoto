504f53d5220e55894b1feb6724a00124c93e7934
jonathan <jonathan.voss@uni.edu>
2023-02-08 23:11:55 -0600

    Remove deprecated views

--- kronofoto/archive/models/__init__.py
+++ kronofoto/archive/models/__init__.py
@@ -12,27 +12,3 @@ from .photo import Photo, get_original_path, format_location, PhotoTag
 from .wordcount import WordCount
 from .cutoff import NewCutoff
 from .photosphere import PhotoSphere, PhotoSpherePair, get_photosphere_path
-
-
-class PrePublishPhoto(models.Model):
-    id = models.AutoField(primary_key=True)
-    photo = models.OneToOneField(Photo, on_delete=models.CASCADE)
-
-
-class ScannedPhoto(models.Model):
-    image = models.ImageField(upload_to='uploads/%Y/%m/%d/') # callable that incorporates donor name?
-    donor = models.ForeignKey(Donor, models.PROTECT)
-
-    created = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(
-        Donor, null=True, on_delete=models.SET_NULL, related_name="scanned_photos"
-    )
-    accepted = models.BooleanField(null=True)
-
-
-class PhotoVote(models.Model):
-    photo = models.ForeignKey(ScannedPhoto, on_delete=models.CASCADE, related_name='votes', related_query_name='vote')
-    voter = models.ForeignKey(User, on_delete=models.CASCADE)
-    infavor = models.BooleanField()
-
-
