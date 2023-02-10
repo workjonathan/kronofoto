c85b5b5a1b8ac9e5b36347de9553fad2c7035380
jonathan <jonathan.voss@uni.edu>
2023-02-10 16:39:15 -0600

    Current collection is preserved from add-tag

--- kronofoto/archive/views/addtag.py
+++ kronofoto/archive/views/addtag.py
@@ -20,6 +20,8 @@ class AddTagView(BaseTemplateMixin, LoginRequiredMixin, FormView):
     def dispatch(self, request, photo):
         self.photo = Photo.objects.get(id=photo)
         self.success_url = reverse('kronofoto:addtag', kwargs={'photo': self.photo.id})
+        if request.GET:
+            self.success_url += '?' + request.GET.urlencode()
         return super().dispatch(request)
 
     def form_valid(self, form):
