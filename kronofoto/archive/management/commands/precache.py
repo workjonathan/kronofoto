ccb8930db480c9b9ada2538f746e4fc5a2e349a6
jonathan <jonathan.voss@uni.edu>
2024-02-08 20:43:35 -0600

    Bring resumeable caching from server

--- kronofoto/archive/management/commands/precache.py
+++ kronofoto/archive/management/commands/precache.py
@@ -5,8 +5,12 @@ from ...imageutil import ImageSigner, ImageCacher
 class Command(BaseCommand):
     help = "precache photos"
 
-    def handle(self, *args, **options):
-        for photo in Photo.objects.all().order_by('id'):
-            print(photo.id)
+    def add_arguments(self, parser):
+        parser.add_argument('--year', type=int, default=0)
+        parser.add_argument('--id', type=int, default=0)
+
+    def handle(self, *args, id, year, **options):
+        for photo in (Photo.objects.filter(year__gt=year)|Photo.objects.filter(year=year, id__gte=id)).order_by('year', 'id'):
+            print(photo.year, photo.id)
             ImageSigner(id=photo.id, path=photo.original.name, width=75, height=75).cacher.precache()
             ImageSigner(id=photo.id, path=photo.original.name, width=500, height=500).cacher.precache()
