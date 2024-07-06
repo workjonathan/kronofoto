5cdad1e41501aa881a9da7073b33867d32faec2b
jonathan <jonathan.voss@uni.edu>
2024-07-05 23:43:38 -0500

    more type annotations

--- kronofoto/archive/geocoder.py
+++ kronofoto/archive/geocoder.py
@@ -3,15 +3,19 @@ from .models import CSVRecord, Location, Photo
 from geocoding import geom
 from geocoding.google import Geocoder, GeocodeError
 import logging
+from typing import Any, Optional, Union, Dict, List, Protocol
 
+class GeocoderProtocol(Protocol):
+    def geocode(self, description: str) -> Any:
+        pass
 
 class CachingGeocoder:
-    def __init__(self, geocoder):
+    def __init__(self, geocoder: GeocoderProtocol):
         self.geocoder = geocoder
 
-    def geocode(self, description):
+    def geocode(self, description: str) -> geom.Location:
         try:
-            location = Location.objects.locate(description=description)
+            location: Any = Location.objects.locate(description=description)
             return geom.Location(centroid=location.location_point, bounds=location.location_bounds)
         except Location.DoesNotExist:
             location = self.geocoder.geocode(description=description)
@@ -25,10 +29,10 @@ class CachingGeocoder:
 
 
 class CSVGeocoder:
-    def __init__(self, geocoder=CachingGeocoder(geocoder=Geocoder(key=settings.GOOGLE_MAPS_KEY))):
+    def __init__(self, geocoder: GeocoderProtocol=CachingGeocoder(geocoder=Geocoder(key=settings.GOOGLE_MAPS_KEY))):
         self.geocoder = geocoder
 
-    def geocode_from_csv_data(self):
+    def geocode_from_csv_data(self) -> None:
         for photo in Photo.objects.exclude_geocoded():
             try:
                 description = photo.location(with_address=True, force_country=True)
