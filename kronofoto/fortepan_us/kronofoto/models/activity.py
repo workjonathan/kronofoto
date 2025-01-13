36f0e58b5a1ce15f0c0f4cd74d1344b1d25fbd5f
jonathan <jonathan.voss@uni.edu>
2025-01-13 13:23:57 -0600

    notes about complicated get_or_create_by_profile problem

--- kronofoto/fortepan_us/kronofoto/models/activity.py
+++ kronofoto/fortepan_us/kronofoto/models/activity.py
@@ -130,8 +130,8 @@ class LdIdQuerySet(models.QuerySet["LdId"]):
         except self.model.DoesNotExist:
             raise NotImplementedError
             # After some consideration, we have determined that loading objects in this way is not a good idea.
-            # It is a hack to check the domains and we don't want to invent a signature system to be include
-            # with all get requests.
+            # It is a hack to check the domains and we don't want to invent a signature system to include
+            # with all get reponses.
             # It seems acceptable to create an empty or undefined or poorly defined object in the event that we do not
             # already know the object. Hopefully the object will be discovered eventually via authenticated Activities
             # or through loading/refreshing an actor's collections.
@@ -139,6 +139,15 @@ class LdIdQuerySet(models.QuerySet["LdId"]):
             # an actor's collections. If we accept the attributedTo field as reliable, we could receive a Create
             # activity that references a LD-ID for a place or donor that we have not seen. That object could be loaded
             # from anywhere and attribute the object to any actor, which allows for spoofing.
+
+            # On the other hand, donors cannot be created without archives. Solutions:
+            # * Domain check hack
+            # * Making an "unknown archive"
+            # * Making archives nullable would produce a lot of type errors, but default manager could filter them.
+            #   * photo.donor.archive could still be a problem
+            # * Unprocessed inbox table for storing incomplete data. Potentially very confusing if create/delete happen
+            #   before finding the rest of the necessary data. CPS transform with defunctonalization make this possible.
+            # Domain hack or unknown archive seem least painful.
             object = requests.get(
                 ld_id,
                 headers={
