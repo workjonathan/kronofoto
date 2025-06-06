200c3959af8970df23f5ad302b6972ff75a9d728
jonathan <jonathan.voss@uni.edu>
2025-06-06 14:46:16 -0500

    docstring for photosphere

--- kronofoto/fortepan_us/kronofoto/models/photosphere.py
+++ kronofoto/fortepan_us/kronofoto/models/photosphere.py
@@ -15,13 +15,28 @@ from django.http import QueryDict
 
 
 class IncompleteGPSInfo(Exception):
+    """Exif GPS extraction can either return GPS coordinates or raise this
+    exception.
+    """
     pass
 
 
 def get_photosphere_path(instance: "PhotoSphere", filename: str) -> str:
+    """Get a storage path for a photosphere image.
+
+    Args:
+        instance (PhotoSphere): A PhotoSphere model record.
+        filename (str): The file's name.
+
+    Returns:
+        str: The storage path for this PhotoSphere image.
+    """
     return path.join("photosphere", "{}.jpg".format(instance.uuid))
 
 class PhotoSphereSetData(models.Model):
+    """Abstract Base Model for Sets and Tours, which both have names and
+    descriptions.
+    """
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(blank=True)
 
@@ -32,15 +47,22 @@ class PhotoSphereSetData(models.Model):
         return self.name
 
 class PhotoSphereTour(PhotoSphereSetData, models.Model):
+    """PhotoSpheres can belong to a Tour. These are typically used to group
+    PhotoSpheres by area. When a user is in a Tour, they can conveniently jump
+    between Sets that have Photos in this Tour.
+    """
     sets = models.ManyToManyField("kronofoto.MainStreetSet", through="kronofoto.TourSetDescription")
 
-
 class MainStreetSet(PhotoSphereSetData, models.Model):
+    """PhotoSpheres can belong to a set. These are typically used to group
+    PhotoSpheres by decade, but other schemes would be fine.
+    """
     tours = models.ManyToManyField("kronofoto.MainStreetSet", through="kronofoto.TourSetDescription")
     def get_absolute_url(self) -> str:
         return reverse("kronofoto:mainstreet-detail", kwargs={"pk": self.pk})
 
 class TourSetDescription(models.Model):
+    """This model allows overriding the Set description for a given Tour."""
     tour = models.ForeignKey(PhotoSphereTour, null=False, on_delete=models.CASCADE)
     set = models.ForeignKey(MainStreetSet, null=False, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
@@ -57,6 +79,13 @@ class TourSetDescription(models.Model):
 
 
 class PhotoSphere(models.Model):
+    """PhotoSpheres are 360 photos. They can have 0 or more Photo objects
+    linked. They can optionally also be linked to 0 or more other PhotoSpheres.
+    They have a latitude and longitude so that they can be placed on a map.
+    They also have a heading which indicates which way is north inside the
+    PhotoSphere. This is needed to make arrows point in the correct direction to
+    other photospheres.
+    """
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=512, blank=False)
     description = models.TextField()
@@ -87,6 +116,16 @@ class PhotoSphere(models.Model):
 
     @staticmethod
     def decimal(*, pos: Tuple[float, float, float], ref: str) -> float:
+        """Exif annoyingly gives lat/lon as degree-minute-seconds and N S W E.
+        This function converts these to one floating point number.
+
+        Args:
+            pos (float, float, float): A tuple representing degrees, minutes, seconds respectively. All numbers are >= 0.
+            ref (str): N, S, E, or W.
+
+        Returns:
+            float: A floating point number incorporating all these numbers ranging from -180 to 180 for W/E or -90 to 90 for N/S.
+        """
         degrees = pos[0]
         minutes = pos[1]
         seconds = pos[2]
@@ -94,6 +133,12 @@ class PhotoSphere(models.Model):
         return float(degrees + minutes / 60) * (-1 if ref in ("S", "W") else 1)
 
     def exif_location(self) -> Point:
+        """Get the WGS84 Point representing the location of the 360 image
+        according to the EXIF data.
+
+        Returns:
+            Point: A WGS84 Point.
+        """
         contents = self.image.read()
         img = Image.open(BytesIO(contents))
         exif_data = img.getexif()
@@ -120,6 +165,11 @@ class PhotoSphere(models.Model):
         raise IncompleteGPSInfo
 
     def get_absolute_url(self) -> str:
+        """The canonical user facing URL for this 360 image.
+
+        Returns:
+            str: The URL for this PhotoSphere.
+        """
         query = QueryDict(mutable=True)
         query["id"] = str(self.pk)
         return "{}?{}".format(reverse("kronofoto:mainstreetview"), query.urlencode())
@@ -129,6 +179,7 @@ class PhotoSphere(models.Model):
 
 
 class PhotoSphereInfo(models.Model):
+    "Used to put an Info marker over an interesting detail in a PhotoSphere."
     photosphere = models.ForeignKey(PhotoSphere, on_delete=models.CASCADE, null=False)
     text = models.TextField(blank=False, null=False)
     yaw = models.FloatField(
@@ -148,6 +199,9 @@ class PhotoSphereInfo(models.Model):
 
 
 class PhotoSpherePair(models.Model):
+    """A Through model linking a Photo and PhotoSphere.
+    This stores the orientation of the Photo in the PhotoSphere.
+    """
     photo = models.ForeignKey(
         "Photo",
         on_delete=models.CASCADE,
