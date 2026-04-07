86e63f3fad67711c1d7e614e51ef69b2972312e1
jonathan <jonathan.voss@uni.edu>
2026-04-06 21:19:43 -0500

    add admin link to map

--- kronofoto/fortepan_us/arctic/apps.py
+++ kronofoto/fortepan_us/arctic/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CtdaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fortepan_us.ctda'
+    name = 'fortepan_us.arctic'
