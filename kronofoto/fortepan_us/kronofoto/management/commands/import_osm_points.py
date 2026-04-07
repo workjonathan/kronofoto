14fc49c5c62288a94af06e09e5f46e515d1a5fd8
jonathan <jonathan.voss@uni.edu>
2026-04-06 20:11:50 -0500

    osm import scripts

--- kronofoto/fortepan_us/kronofoto/management/commands/import_osm_points.py
+++ kronofoto/fortepan_us/kronofoto/management/commands/import_osm_points.py
@@ -27,11 +27,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--geojson', required=True)
-        parser.add_argument('--admin_level', required=True, type=str)
         parser.add_argument("--parent_type", required=True, type=int)
         parser.add_argument("--new_type", required=True, type=int)
 
-    def handle(self, *args, geojson, parent_type, admin_level, new_type, **options):
+    def handle(self, *args, geojson, parent_type, new_type, **options):
        # m = Place.objects.get(name="Mongolia")
        # for p in Place.objects.filter(parent=m):
        #     print(p)
@@ -49,16 +48,11 @@ class Command(BaseCommand):
             new_places = []
             for feature in data['features']:
                 properties = feature['properties']
-                if admin_level != properties['admin_level']:
-                    continue
                 name = properties['name']
                 import re
                 tags = dict(re.findall(r'"([^"]*)"=>"([^"]*)"', properties['other_tags'])) if properties['other_tags'] else {}
-                if feature['geometry']['type'] == 'Polygon':
-                    polygons = [shapely.Polygon(feature['geometry']['coordinates'][0], feature['geometry']['coordinates'][1:])]
-                elif feature['geometry']['type'] == 'MultiPolygon':
-                    polygons = [shapely.Polygon(coords[0], coords[1:]) for coords in feature['geometry']['coordinates']]
-                geom = shapely.MultiPolygon(polygons)
+                print(feature['geometry']['coordinates'])
+                geom = shapely.Point(feature['geometry']['coordinates'])
                 new_places.append((name, geom, tags))
                 print(geom.centroid)
             print(new_places)
